4ef4feabdbe0243af5d1b4f16942dc9d2fac11b5
Tuan Long <tuanlongdinh.ka@gmail.com>
2021-10-11 13:24:12 +0700

    create table with db_schema

--- Packt/HelloWorld/Setup/Patch/Data/AddProductAttribute.php
+++ Packt/HelloWorld/Setup/Patch/Data/AddProductAttribute.php
@@ -5,6 +5,7 @@ use Magento\Eav\Setup\EavSetup;
 use Magento\Eav\Setup\EavSetupFactory;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
 use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
 
 class AddProductAttribute implements DataPatchInterface
 {
